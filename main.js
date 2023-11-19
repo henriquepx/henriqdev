df6f3b226a1726f602e09fcf803d1b15f3d9be5a
Henrique <henriquepinheiroxavier@gmail.com>
2023-11-19 15:35:42 -0300

    feat: emailjs configuration

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
+// TITLE REFRESH
 const titleRefresh = document.getElementById('title');
-
 titleRefresh.addEventListener('click', function () {
     location.reload();
 });
+
+// FORM - EMAILJS
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const formData = new FormData(form);
+
+      emailjs.sendForm('service_webqtko', 'template_kxj8ru8', form.current, '6XCPVQDlEI-egjo2f')
+        .then((result) => {
+          console.log(result.text);
+        }, (error) => {
+          console.log(error.text);
+        });
+      form.reset();
+    });
+  });
