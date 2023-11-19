b33c8ada9d82e0fea85f3b6e640187cf71810101
Henrique <henriquepinheiroxavier@gmail.com>
2023-11-19 18:50:20 -0300

    feat: skills and description with the animation tabcontent

--- main.js
+++ main.js
@@ -20,4 +20,45 @@ document.addEventListener('DOMContentLoaded', function () {
         });
       form.reset();
     });
+});
+  
+// BACK TO TOP
+document.addEventListener('DOMContentLoaded', function () {
+    const backToTopButton = document.getElementById('backToTop');
+  
+    // Adiciona um evento de clique à seta
+    backToTopButton.addEventListener('click', function () {
+      // Scroll suave até o topo da página
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+  
+    // Adiciona um observador de rolagem para mostrar ou ocultar a seta conforme necessário
+    window.addEventListener('scroll', function () {
+      if (window.scrollY > 300) {
+        backToTopButton.classList.add('show');
+      } else {
+        backToTopButton.classList.remove('show');
+      }
+    });
+});
+  
+// LANGUAGE SKILLS CONTENT
+const menuLanguage = document.querySelectorAll('.menulanguage');
+const contentLanguage = document.querySelectorAll('.contentlanguage');
+
+function activeTab(index) {
+  contentLanguage.forEach((section) => {
+    section.classList.remove('ativo');
   });
+  menuLanguage.forEach((section) => {
+    section.classList.remove('ativo');
+  });
+    contentLanguage[index].classList.add('ativo');
+    menuLanguage[index].classList.add('ativo');
+}
+
+menuLanguage.forEach((itemMenu, index) => {
+  itemMenu.addEventListener('click', () => {
+    activeTab(index);
+  })
+})
