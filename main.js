58b0329846e769e4e2457be675cf86b2d607ef2a
Henrique <henriquepinheiroxavier@gmail.com>
2024-01-26 19:59:43 -0300

    feat: adjusts in all sections and organize the nexts features

--- main.js
+++ main.js
@@ -56,27 +56,6 @@ closeIcon.addEventListener("click", () => {
     mobileMenuContent.classList.remove("ativo");
 });
 
-// ANIMATION LANGUAGES
-const scrollers = document.querySelectorAll(".scroller");
-
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
-}
-
-function addAnimation() {
-  scrollers.forEach((scroller) => {
-    scroller.setAttribute("data-animated", true);
-
-    const scrollerInner = scroller.querySelector(".scroller__inner");
-    const scrollerContent = Array.from(scrollerInner.children);
-
-    scrollerContent.forEach((item) => {
-      const duplicatedItem = item.cloneNode(true);
-      duplicatedItem.setAttribute("aria-hidden", true);
-      scrollerInner.appendChild(duplicatedItem);
-    });
-  });
-}
 
 // ANIMA AO SCROLL
 const sectionsToAnimate = document.querySelectorAll('.sectionanima');
