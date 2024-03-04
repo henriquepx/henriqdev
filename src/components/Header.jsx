b94310dca37e5861b2c962ef1f32cc68996c370a
Henrique <henriquepinheiroxavier@gmail.com>
2024-03-03 22:25:31 -0300

    feat: header

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import './Header.css'
+import { IoCodeSlash } from "react-icons/io5";
+import { VscLayoutSidebarLeft, VscLayoutPanelOff, VscLayoutSidebarRightOff, VscLayout, VscChromeMinimize, VscChromeRestore, VscChromeClose } from "react-icons/vsc";
+
+
+const Header = () => {
+    return (
+        <header className='container'>
+            <div className='header1'>
+                <IoCodeSlash size={20} />
+                <p>File</p>
+                <p>Edit</p>
+                <p>Selection</p>
+                <p>View</p>
+                <p>Go</p>
+                <p>Run</p>
+                <p>Terminal</p>
+                <p>Help</p>
+
+            </div>
+            <div className='header2'>
+                <VscLayoutSidebarLeft />
+                <VscLayoutPanelOff />
+                <VscLayoutSidebarRightOff />
+                <VscLayout />
+                <div>
+                    <VscChromeMinimize />
+                    <VscChromeRestore />
+                    <VscChromeClose />
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
