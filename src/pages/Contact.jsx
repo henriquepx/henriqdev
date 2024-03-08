9eb4a0fa5c1e01220d388336fc98e12a10828cdd
Henrique <henriquepinheiroxavier@gmail.com>
2024-03-08 17:54:43 -0300

    feat(styles): section contact with social mdias

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,6 @@
 import './Contact.css';
 import TabFiles from './Layout/TabFiles';
-import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
-import { MdArticle } from "react-icons/md";
-import Form from '../components/Form';
+// import Form from '../components/Form';
 
 const Contact = () => {
     return (
@@ -14,15 +12,15 @@ const Contact = () => {
                     <div className='contactways'>
                         <div className='socialmediascontact'>
                             <p className='opensocial'>.socialmedias {`{`}</p>
-                            <p className='midiastext'>      github: @henriquepx <FaGithub /></p>
-                            <p className='midiastext'>      linkedin: Henrique Pinheiro <FaLinkedin /></p>
-                            <p className='midiastext'>      blog: devinsights <MdArticle /></p>
-                            <p className='midiastext'>      whatsapp: send a massage <FaWhatsapp /></p>
-                            <p className='midiastext'>      instagram: henriquepx <FaInstagram /></p>
+                            <p className='midiastext'>&nbsp;&nbsp;&nbsp;github: <a href="https://github.com/henriquepx" target='_blank'>@henriquepx;</a></p>
+                            <p className='midiastext'>&nbsp;&nbsp;&nbsp;linkedin: <a href="https://www.linkedin.com/in/henriquepinheiroxavier/" target='_blank'>Henrique Pinheiro;</a></p>
+                            <p className='midiastext'>&nbsp;&nbsp;&nbsp;blog:  <a href="https://dev-insights-self.vercel.app/" target='_blank'>devinsights;</a></p>
+                            <p className='midiastext'>&nbsp;&nbsp;&nbsp;whatsapp:  <a href="https://api.whatsapp.com/send?phone=5521964823939&text=Ol%C3%A1,%20Henrique.%20Tenho%20uma%20ideia%20de%20trabalho%20e%20voc%C3%AA%20%C3%A9%20o%20Desenvolvedor%20que%20eu%20preciso!" target='_blank'>send a massage;</a></p>
+                            <p className='midiastext'>&nbsp;&nbsp;&nbsp;instagram: <a href="https://www.instagram.com/henriquepxx/" target='_blank'>henriquepx;</a></p>
                             <p className='closesocial'>{`}`}</p>
                         </div>
                         <div className="containerform">
-                            <Form />
+                            {/* <Form /> */}
                         </div>
                     </div>
                 </div>
