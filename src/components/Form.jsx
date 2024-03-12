1e700bde0b11942409f91430a52eef893af706cb
Henrique <henriquepinheiroxavier@gmail.com>
2024-03-11 21:50:03 -0300

    feat: contact structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,113 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-const FormContainer = styled.div`
+const ContainerForm = styled.div`
+   width: 100%;
+   background-color: #2e2e2e; /* Cor do background */
+`
 
+const FormStyle = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+    color: #fff; /* Cor da fonte */
+`
 
+const LabelForm = styled.label`
+    margin-bottom: 7px;
+    font-family: 'Montserrat', monospace;
+    color: #fff; /* Cor da fonte */
+`
+
+const InputStyle = styled.input`
+    margin-bottom: 14px;
+    border-radius: 4px;
+    height: 40px;
+    color: #fff;
+    width: 100%; 
+    padding: 15px 8px;
+    font-family: 'Montserrat', monospace;
+    background-color: transparent;
+    border: 1px solid #fff; /* Cor da borda */
+`
+
+const TextareaStyle = styled.textarea`
+    margin-bottom: 14px;
+    border-radius: 4px;
+    font-family: 'Montserrat', monospace;
+    border: 0;
+    padding: 8px;
+    background-color: transparent;
+    border: 1px solid #fff; /* Cor da borda */
+    height: 200px;
+    width: 100%;
+    color: #fff;
+    resize: none;
+`
+
+const ButtonSubmitForm = styled.button`
+    height: 34px;
+    border-radius: 4px;
+    border: 0;
+    cursor: pointer;
+    background-color: #181818; /* Cor do header, footer e aside */
+    font-size: 1rem;
+    margin: 0 auto;
+    width: 30%;
+    color: #fff;
+    font-family: 'Montserrat', monospace;
+`
+
+const NameEmail = styled.div`
+    display: flex;
+    gap: 50px;
+    justify-content: space-between;
+    @media (max-width: 600px) {
+        flex-direction: column;
+        gap: 0px;
+    }
+`
+
+const EveryInput = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
 `
 
 const Form = () => {
+    return (
+        <ContainerForm>
+            <FormStyle>
+                <NameEmail>
+                    <EveryInput>
+                        <LabelForm htmlFor="user_name">Nome</LabelForm>
+                        <InputStyle 
+                            type="text"
+                            placeholder="Digite seu nome"
+                            name="from_name"
+                            id="user_name"
+                        />
+                    </EveryInput>
+                    <EveryInput>
+                    <LabelForm htmlFor="user_email">E-mail:</LabelForm>
+                    <InputStyle 
+                        type="text"
+                        placeholder="Digite seu e-mail"
+                        name="reply_to"
+                        id="user_email"
+                    />
+                    </EveryInput>
+                </NameEmail>
+                <LabelForm htmlFor="message">Mensagem</LabelForm>
+                <TextareaStyle 
+                    placeholder="Deixe sua mensagem"
+                    name="message"
+                    id="message"
+                />
 
-  return (
-    <FormContainer>
-      <form>
-        <h1>Form</h1>
-      </form>
-    </FormContainer>
-  );
+                <ButtonSubmitForm type="submit" value="Send">Enviar</ButtonSubmitForm>
+            </FormStyle>
+        </ContainerForm>
+    );
 }
 
 export default Form;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import TabFiles from './Layout/TabFiles';
-// import Form from '../components/Form';
+import Form from '../components/Form';
+import { FaFile, FaGithub, FaLinkedin, FaWhatsapp } from 'react-icons/fa'
 
 const ContainerContact = styled.div`
     height: calc(100vh - 6.3vh);
@@ -9,6 +10,7 @@ const ContainerContact = styled.div`
     flex-direction: column;
     align-items: center;
     margin-left: 55px;
+    background-color: #2e2e2e;
 
     @media (max-width: 450px) {
         margin-left: 50px;
@@ -16,85 +18,69 @@ const ContainerContact = styled.div`
 `;
 
 const ContactSize = styled.div`
-    position: absolute;
-    left: 75px;
-    top: 60px;
+    margin-top: 6rem;
     display: flex;
-
-    @media (max-width: 450px) {
-        left: 50px;
-        top: 40px;
-        padding: 1rem;
-    }
-`;
-
-const ContactMidias = styled.div`
-    h1 {
-        color: #999898;
-        font-family: 'Montserrat', sans-serif;
-        font-size: 1.2rem;
-        margin-bottom: 1rem;
-    }
-`;
-
-const SocialMediasContact = styled.div`
-    counter-reset: line;
-    line-height: 2rem;
-
+    flex-direction: column-reverse;
+    gap: 20px;
+    color: #fff;
     p {
-        font-family: 'Montserrat', sans-serif;
-        color: #969696;
-        display: flex;
-        align-items: center;    
-        gap: 10px;
-        font-size: 1.2rem;
-
-        a {
-            color: #d1d1d1;
-            font-size: 1.2rem;
-
-            &:hover {
-                text-decoration: underline;
-            }
-        }
-
-        @media (max-width: 450px) {
-            font-size: .85rem;
-        }
+        text-align: center;
     }
 `;
 
-const SocialMediasContactLink = styled.a`
-    color: #ebebeb;
-    font-size: 1.2rem;
-
-    &:hover {
-        text-decoration: underline;
-    }
+const UlContact = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
 `;
+
+const ALinkContact = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    padding: .5rem 1rem;
+    width: 220px;
+    border-radius: 10px;
+    border: 3px solid #7a7a7a;
+    p {
+        color: #fff; 
+    }
+`
 
 const Contact = () => {
     return (
         <ContainerContact>
             <TabFiles activeTab='Contact' />
             <ContactSize>
-                <ContactMidias>
-                    <h1>Entre em contato de duas formas:</h1>
+
+                    <nav>
+                        <UlContact>
+                            <ALinkContact href="#" target='_blank' rel='noreferre'>
+                                <FaLinkedin size={18} color='#fff'/>
+                                <p>LinkedIn</p>
+                            </ALinkContact>
+                            <ALinkContact href="#" target='_blank' rel='noreferre'>
+                                <FaGithub size={18} color='#fff'/>
+                                <p>Github</p>
+                            </ALinkContact>
+                            <ALinkContact href="#" target='_blank' rel='noreferre'>
+                                <FaWhatsapp size={18} color='#fff'/>
+                                <p>Whatsapp</p>
+                            </ALinkContact>
+                            <ALinkContact href="#" target='_blank' rel='noreferre'>
+                                <FaFile size={18} color='#fff'/>
+                                <p>Blog</p>
+                            </ALinkContact>
+                        </UlContact>
+                    </nav>
+                    <p>or</p>
                     <div>
-                        <SocialMediasContact>
-                            <p>.socialmedias {`{`}</p>
-                            <p>&nbsp;&nbsp;&nbsp;github: <SocialMediasContactLink href="https://github.com/henriquepx" target='_blank'>@henriquepx;</SocialMediasContactLink></p>
-                            <p>&nbsp;&nbsp;&nbsp;linkedin: <SocialMediasContactLink href="https://www.linkedin.com/in/henriquepinheiroxavier/" target='_blank'>Henrique Pinheiro;</SocialMediasContactLink></p>
-                            <p>&nbsp;&nbsp;&nbsp;blog:  <SocialMediasContactLink href="https://dev-insights-self.vercel.app/" target='_blank'>devinsights;</SocialMediasContactLink></p>
-                            <p>&nbsp;&nbsp;&nbsp;whatsapp:  <SocialMediasContactLink href="https://api.whatsapp.com/send?phone=5521964823939&text=Ol%C3%A1,%20Henrique.%20Tenho%20uma%20ideia%20de%20trabalho%20e%20voc%C3%AA%20%C3%A9%20o%20Desenvolvedor%20que%20eu%20preciso!" target='_blank'>send a message;</SocialMediasContactLink></p>
-                            <p>&nbsp;&nbsp;&nbsp;instagram: <SocialMediasContactLink href="https://www.instagram.com/henriquepxx/" target='_blank'>henriquepx;</SocialMediasContactLink></p>
-                            <p>{`}`}</p>
-                        </SocialMediasContact>
-                        <div>
-                            {/* <Form /> */}
-                        </div>
+                        <Form />
                     </div>
-                </ContactMidias>
+
             </ContactSize>
         </ContainerContact>
     );
