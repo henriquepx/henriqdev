8a2ff46b8184503d71b4213554dc3541a84b7e27
Henrique <henriquepinheiroxavier@gmail.com>
2024-03-19 11:24:51 -0300

    feat: file-saver to download my cv that i create in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import TabFiles from './Layout/TabFiles';
+import { FaFilePdf } from 'react-icons/fa6';
+import { saveAs } from 'file-saver';
 
 const HomeContainer = styled.div`
     position: relative;
@@ -64,8 +66,27 @@ const Faixa = styled.div`
         color: #dad9d9;
     }
 `
+const CV = styled.a`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    color: #b9b9b9;
+    background-color: #222222;
+    border: 1px solid #8b8b8b;
+    border-radius: 15px;
+    padding: .3rem 2rem;
+    margin-top: 1rem;
+    cursor: pointer;
+`
 
 const Home = () => {
+
+    const handleDownloadCV = () => {
+        const cvFileName = 'CV - Henrique.pdf';
+        const cvPath = `/${cvFileName}`;
+        saveAs(cvPath, cvFileName);
+    };
+
     return (
         <HomeContainer>
             <TabFiles activeTab='Home' />
@@ -74,6 +95,10 @@ const Home = () => {
                 <h2>Desenvolvedor Front-end</h2>
                 <DescriptionHome>Apaixonado por criar interfaces web robustas e escaláveis que proporcionam experiências excepcionais ao usuário.</DescriptionHome>
                 <p>Disponível para trabalhos.</p>
+                <CV onClick={handleDownloadCV} name="CV" download>
+                    <FaFilePdf />
+                    CV
+                </CV>
                 <Faixa>
                     <p>Disponível para trabalho.</p>
                 </Faixa>
