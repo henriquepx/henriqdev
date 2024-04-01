87936dbc3968fc7b299719b2e89a74feac5a6af2
Henrique <henriquepinheiroxavier@gmail.com>
2024-04-01 17:55:20 -0300

    feat: home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,25 +12,30 @@ const moveLeft = keyframes`
         transform: translateX(0);
     }
 `;
-
 const moveTextTitle = keyframes`
     0% {
-        transform: translateX(-200px);
+        transform: translateY(200px);
     }
     100% {
         transform: translateX(0);
     }
 `;
-
 const moveTextSubitle = keyframes`
     0% {
-        transform: translateX(-200px);
+        transform: translateY(200px);
     }
     100% {
         transform: translateX(0);
     }
 `;
-
+const moveTexts = keyframes`
+    0% {
+        transform: translateY(-200px);
+    }
+    100% {
+        transform: translateX(0);
+    }
+`;
 const HomeContainer = styled.div`
     position: relative;
     height: calc(100vh - 6.3);
@@ -38,7 +43,7 @@ const HomeContainer = styled.div`
     overflow: hidden;
 `;
 const HomeSize = styled.div`
-    margin: 5rem 1.2rem 5rem 5rem;
+    margin: 5.7rem 1.2rem 5rem 5rem;
 
 `
 const InfoHome = styled.div`
@@ -46,8 +51,9 @@ const InfoHome = styled.div`
     justify-content: space-between;
     flex-wrap: wrap;
     gap: 20px;
+    animation: ${moveTexts} 1s forwards;
     @media (max-width: 800px) {
-        margin-bottom: 3rem;
+        margin-bottom: 0.6rem;
     }
 `;
 const InfoLinks = styled.div`
@@ -60,6 +66,10 @@ const InfoLinks = styled.div`
     }
 `;
 const InfoAvailable = styled.div`
+    text-align: right;
+    @media (max-width: 937px) {
+        text-align: left;
+    }
     p {
         font-family: "Urbanist", sans-serif;
         color: #fff;
@@ -99,10 +109,13 @@ const InfoTitle = styled.div`
 const TitleDev = styled.h1`
     font-size: 14vw;
     text-transform: uppercase;
-    color: #dadada;
+    color: #979797;
     font-family: "Fjalla One", sans-serif;
     display: block;
     animation: ${moveTextSubitle} 1s forwards;
+    &:hover {
+        color: #ffffff;
+    }
     @media (max-width: 800px) {
         display: none;
     }
@@ -117,15 +130,18 @@ const TitleDev2 = styled(TitleDev)`
 const TitleFrontend = styled.h2`
     font-size: 18vw;
     text-transform: uppercase;
-    color: #dadada;
+    color: #979797;
     font-family: "Fjalla One", sans-serif;
     animation: ${moveTextTitle} 1s forwards;
+    margin-top: -1.5rem;
+    &:hover {
+        color: #ffffff;
+    }
     @media (max-width: 1280px) {
         font-size: 21vw;
         margin-top: 1rem;
     }
 `
-
 // const Faixa = styled.div`
 //     z-index: 200;
 //     position:fixed;
@@ -148,13 +164,17 @@ const CV = styled.a`
     align-items: center;
     gap: 10px;
     color: #b9b9b9;
-    background-color: #222222;
     border: 1px solid #8b8b8b;
     border-radius: 15px;
     width: 120px;
     padding: .3rem 2rem;
     margin-top: .5rem;
     cursor: pointer;
+    margin-left: auto;
+    @media (max-width: 937px) {
+        margin-left: 0;
+        margin-right: auto;
+    }
 `
 
 
@@ -179,14 +199,13 @@ const Home = () => {
                         <InstructionsSobre><span>SOBRE</span>Me chamo Henrique Pinheiro. Sou um Desenvolvedor com foco em construções web robustas e interativas utilizando as tecnologias mais modernas do mercado.</InstructionsSobre>
                     </div>
                     <InfoLinks>
-                        <a href="#" target='_blank' rel='noreferrer'>blog: devinsights</a>
                         <a href="#" target='_blank' rel='noreferrer'>henriquepinheiroxavier@gmail.com</a>
                     </InfoLinks>
                     <InfoAvailable>
-                        <p>Disponível para trabalho & Freelances</p>
-                        <CV onClick={handleDownloadCV} name="CV" download>
-                            <FaFilePdf />
+                            <p>Disponível para trabalho <br />& Freelance</p>
+                            <CV onClick={handleDownloadCV} name="CV" download>
                             CV
+                            <FaFilePdf />
                         </CV>
                     </InfoAvailable>
                 </InfoHome>
